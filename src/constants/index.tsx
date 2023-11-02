9ece7d0cc84e7170840059b54da9a538b6c8e60b
Allgud <allgud83@gmail.com>
2023-11-02 22:17:05 +0300

    exact fixed flags render

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,8 +1,15 @@
+import rus from '../assets/img/russia.svg'
+import gb from '../assets/img/great_britain.svg'
+import de from '../assets/img/deutschland.svg'
+import spa from '../assets/img/spain.svg'
+import ita from '../assets/img/italia.svg'
+import pol from '../assets/img/poland.svg'
+
 export const LANGUAGES_OPTIONS = [
-  { id: 1, title: "Русский", value: "ru", checked: false, flag_icon: "public/flags/russia.svg" },
-  { id: 2, title: "Английский", value: "en", checked: false, flag_icon: "src/assets/flags/great_britain.svg" },
-  { id: 3, title: "Немецкий", value: "de", checked: false, flag_icon: "src/assets/flags/deutschland.svg" },
-  { id: 4, title: "Польский", value: "po", checked: false, flag_icon: "src/assets/flags/poland.svg" },
-  { id: 5, title: "Испанский", value: "spa", checked: false, flag_icon: "src/assets/flags/spain.svg" },
-  { id: 6, title: "Итальянский", value: "it", checked: false, flag_icon: "src/assets/flags/italia.svg" },
+  { id: 1, title: "Русский", value: "ru", checked: false, flag_icon: rus },
+  { id: 2, title: "Английский", value: "en", checked: false, flag_icon: gb },
+  { id: 3, title: "Немецкий", value: "de", checked: false, flag_icon: de },
+  { id: 4, title: "Польский", value: "po", checked: false, flag_icon: pol },
+  { id: 5, title: "Испанский", value: "spa", checked: false, flag_icon: spa },
+  { id: 6, title: "Итальянский", value: "it", checked: false, flag_icon: ita },
 ]
